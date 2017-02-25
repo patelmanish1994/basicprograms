b16de71a227b1aee1f8a0569eb2c0e8f03b42791
patelmanish1994 <manish007>
2017-02-25 12:16:50 +0530

    arrays in python second programs

---
+++ arrays.py
@@ -0,0 +1,21 @@
+vec= [3, 6, -9,-9,9,9]
+abs(x) for x in vec
+
+
+print vec
+
+
+##x.reverse()
+##
+##y=len(x)
+##print x
+##
+##
+####from array import array
+####
+##
+##x=array('i',[2,4,6,8])
+##u=x/2
+###x=array.typecode
+##
+##print u
