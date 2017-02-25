4d8a7d05a2b935475b209095fd643406e366b12d
patelmanish1994 <manishkumarpatel1994@gmail.com>
2017-02-25 22:48:09 +0530

    trignometry
    trignometry example

--- trignometry.py
+++ trignometry.py
@@ -7,7 +7,9 @@ fmt = '  '.join(['%7.2f'] * 5)
 
 for deg in range(0, 361, 30):
     rad = math.radians(deg)
-    if deg in (90, 270):
+    if deg in (90, 270
+               
+        
         t = float('inf')
     else:
         t = math.tan(rad)
