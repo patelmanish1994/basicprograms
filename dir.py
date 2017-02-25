4d09d51aa5997c3f24f0ab27f7713b90e90af505
patelmanish1994 <manish007>
2017-02-25 12:34:50 +0530

    some more

---
+++ dir.py
@@ -0,0 +1,19 @@
+import os, sys
+
+# Open a file
+#provide the path of the directoryin string format
+
+path = "B:"
+
+#this will return he list of files in the directory
+directory = os.listdir( path )
+
+
+
+#this is the way to print the element of list line by line
+
+#here files is the variable randomly it can be x,y anything
+
+for files in directory: # This would print all the files in directories in seprate lines
+
+   print files
---
+++ fabnocci.py
@@ -0,0 +1,13 @@
+
+number1=input("enter the first number  series : ")
+number2=input("enter the second number series : ")
+
+number_of_terms=input("enter the number of terms required : ")
+print number1
+print number2
+while (number_of_terms!=0):
+    num3=number1+number2
+    number1=number2
+    number2=num3
+    number_of_terms=number_of_terms-1
+    print num3
